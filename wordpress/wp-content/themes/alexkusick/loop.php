bce2d1d367a1f70c17c8b0983eb74fc872708e13
Alex Kusick <Akusick@mcad.edu>
2018-03-28 23:57:56 -0500

    Check-In 10.1
    updates

--- wordpress/wp-content/themes/alexkusick/loop.php
+++ wordpress/wp-content/themes/alexkusick/loop.php
@@ -2,8 +2,8 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
 
 	<article <?php post_class(); ?>>
-		<a href="<?php the_permalink($the_title); ?>">
-			<?php the_title('<h2>','</h2>');?></a>
+		<a href="<?php the_permalink(); ?>">
+			<?php the_title('<h2>','</h2>'); the_post_thumbnail(); ?> </a>
 
 			<?php //the_content(); ?>
 		</article>
